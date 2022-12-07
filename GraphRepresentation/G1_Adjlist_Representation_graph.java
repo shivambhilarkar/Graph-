8136083681bf82a85824d23ef23462b98bf1cc39
Shivam Bhilarkar <shivambhilarkar@gmail.com>
2022-12-07 21:12:18 +0530

    .

---
+++ GraphRepresentation/G1_Adjlist_Representation_graph.java
@@ -0,0 +1,17 @@
+package GraphRepresentation;
+
+public class G1_Adjlist_Representation_graph {
+    public class Edge{
+        int src;
+        int dest;
+        Edge(int src, int dest){
+            this.src = src;
+            this.dest = dest;
+        }
+    }
+
+
+    public static void main(String[] args){
+
+    }
+}
