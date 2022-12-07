e9f7c19ef925955467675dee6bd265afe0528bfc
Shivam Bhilarkar <shivambhilarkar@gmail.com>
2022-12-07 21:44:24 +0530

    Graph Representation using adj Matrix

--- GraphRepresentation/G2_AdjMatrix_Representation_graph.java
+++ GraphRepresentation/G2_AdjMatrix_Representation_graph.java
@@ -9,29 +9,23 @@ public class G2_AdjMatrix_Representation_graph {
             this.nodes = nodes;
             this.matrix = new int[nodes][nodes];
         }
-
         //add edge a -> b
         public void addDirectedEdge(int src, int dest){
             matrix[src][dest] = 1;
         }
-
         //add edge a -> b & b -> a
         public void addUnDirectedEdge(int src, int dest){
             addDirectedEdge(src, dest);
             addDirectedEdge(dest, src);
         }
-
     }
 
-
     public static void main(String[] args){
-
         Graph graph = new Graph(5);
 
         graph.addDirectedEdge(0 ,1);
         graph.addDirectedEdge(1 ,2);
         graph.addDirectedEdge(2 ,3);
         graph.addDirectedEdge(3 ,4);
-
     }
 }
